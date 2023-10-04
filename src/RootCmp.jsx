282db2c9d6863e88716be218e5c137fbdfe16ac8
Alon Lavi <alonlavi7@gmail.com>
2023-10-04 16:05:47 +0300

    update

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,56 +1,57 @@
 import React from 'react'
 import routes from './routes'
-<<<<<<< HEAD
 import { Routes, Route } from 'react-router'
 import { useEffect, useState } from 'react'
 
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
-// import { UserDetails } from './pages/UserDetails'
-=======
+import { UserDetails } from './pages/UserDetails'
 import { NavBar } from './cmps/NavBar'
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
 import { UserDetails } from './pages/UserDetails'
 import { NavLink } from 'react-router-dom'
->>>>>>> refs/remotes/origin/main
 
 export function RootCmp() {
 	const [scrollTop, setScrollTop] = useState(0)
 
-<<<<<<< HEAD
+	// return (
+	// 	<div>
+	// 		<main
+	// 			onScroll={(ev) => {
+	// 				setScrollTop(ev.currentTarget.scrollTop)
+	// 			}}
+	// 			className="home-app-container"
+	// 		>
+	// 			<AppHeader scrollTop={scrollTop} />
+	// 			<Routes>
+	// 				{routes.map((route) => (
+	// 					<Route key={route.path} exact={true} element={route.component} path={route.path} />
+	// 				))}
+	// 				{/* <Route path="user/:id" element={<UserDetails />} /> */}
+	// 			</Routes>
+	// 		</main>
+	// 		<AppFooter />
+	// 	</div>
+	// )
 	return (
 		<div>
+			<AppHeader scrollTop={scrollTop} />
 			<main
 				onScroll={(ev) => {
 					setScrollTop(ev.currentTarget.scrollTop)
 				}}
 				className="home-app-container"
 			>
-				<AppHeader scrollTop={scrollTop} />
+				<NavBar />
 				<Routes>
 					{routes.map((route) => (
 						<Route key={route.path} exact={true} element={route.component} path={route.path} />
 					))}
-					{/* <Route path="user/:id" element={<UserDetails />} /> */}
+					<Route path="user/:id" element={<UserDetails />} />
 				</Routes>
 			</main>
 			<AppFooter />
 		</div>
 	)
-=======
-    return (
-        <div>
-            <AppHeader />
-            <main>
-              <NavBar />
-                <Routes>
-                    {routes.map(route => <Route key={route.path} exact={true} element={route.component} path={route.path} />)}
-                    <Route path="user/:id" element={<UserDetails />} />
-                </Routes>
-            </main>
-            <AppFooter />
-        </div>
-    )
->>>>>>> refs/remotes/origin/main
 }

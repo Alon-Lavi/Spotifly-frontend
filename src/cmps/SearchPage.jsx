4078ab98453f8f39e2457e0805f3c771fa17cf1f
Daniel Kirushin <retrostyae@mail.ru>
2023-10-05 09:20:32 +0300

    SearchPage/css

--- src/cmps/SearchPage.jsx
+++ src/cmps/SearchPage.jsx
@@ -1,8 +1,18 @@
 
-
-
+import { Link } from "react-router-dom"
+import { StationPreview } from "./StationPreview"
+import { stationService } from '../services/station.service.js'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { loadStations, addStation, updateStation, removeStation } from '../store/actions/station.actions.js'
 
 export function SearchPage() {
+
+    const stations = useSelector((storeState) => storeState.stationModule.stations)
+	useEffect(() => {
+		loadStations()
+	}, [])
+
     return (
         <section className="search-page">
             <div className="search-bar">
@@ -11,15 +21,23 @@ export function SearchPage() {
                 <div className="search-input-container">
                  <input
                  name="txt"
-                 value={""}
+                //  value={""}
                  type="text"
                  placeholder="What do you want to listen..."/>
                 </div>
-                <button className="search-button" type="submit">
+                {/* <button className="search-button" type="submit">
                     Search
-                </button>
+                </button> */}
                </form>
             </div>
+            {/* <ul className="station-list">
+        {stations.map((station) => (
+            <li className="station-preview" key={station._id}>
+             <Link to={`station/${station._id}`}>  <StationPreview station={station} /></Link> 
+
+            </li>
+        ))}
+    </ul> */}
         </section>
     )
 }

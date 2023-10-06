d8c3d61f316f087b1bb9b3e7ee7e2c0681d5578f
Daniel Kirushin <retrostyae@mail.ru>
2023-10-06 17:15:21 +0300

    testtt

--- src/cmps/NavBar.jsx
+++ src/cmps/NavBar.jsx
@@ -56,7 +56,6 @@ export function NavBar() {
                                     <span className="library-span">Your Library</span>
                                 </span>
 
-<<<<<<< HEAD
                             </NavLink>
                         </li>
                         <li
@@ -71,36 +70,7 @@ export function NavBar() {
                                          style={{ marginTop: '19px', marginLeft: '55px' }}
                                          >
                                         <path fill="#ffffff" d="M480 480V128a32 32 0 0 1 64 0v352h352a32 32 0 1 1 0 64H544v352a32 32 0 1 1-64 0V544H128a32 32 0 0 1 0-64h352z" /></svg>
-=======
-                    <li className="side-bar-item">
-                        <NavLink to="/library" className="nav-link">
-                            <span className="library">
-                                <svg
-                                    role="img"
-                                    height="24"
-                                    width="24"
-                                    aria-hidden="true"
-                                    viewBox="3 1 24 24"
-                                    data-encore-id="icon"
-                                    className="Svg-sc-ytk21e-0 ldgdZj"
-                                    style={{ color: 'white', marginBottom: '5px' }}
-                                >
-                                    <path d="M3 22a1 1 0 0 1-1-1V3a1 1 0 0 1 2 0v18a1 1 0 0 1-1 1zM15.5 2.134A1 1 0 0 0 14 3v18a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V6.464a1 1 0 0 0-.5-.866l-6-3.464zM9 2a1 1 0 0 0-1 1v18a1 1 2 1 0 2 0V3a1 1 0 0 0-1-1z"></path>
-                                </svg>
-
-                                <span>Your Library</span>
-                            </span>
-
-                        </NavLink>
-                    </li>
-                    {/* <li className="side-bar-item">
-                        <NavLink to="/liked" className="nav-link">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="1.4em" viewBox="1 6 75 24"><mask id="ipSLike0"><path fill="#fff" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="M15 8C8.925 8 4 12.925 4 19c0 11 13 21 20 23.326C31 40 44 30 44 19c0-6.075-4.925-11-11-11c-3.72 0-7.01 1.847-9 4.674A10.987 10.987 0 0 0 15 8Z"></path></mask><path fill="#ffffff" d="M0 0h48v48H0z" mask="url(#ipSLike0)"></path></svg>
-                            Liked Songs
-                        </NavLink>
-                    </li>
->>>>>>> 39f29c10edcfae893eb7faee240dc5b80b36c2e2
-
+                                        
                                 </span>
                             </NavLink>
                         </li>
@@ -146,4 +116,4 @@ export function NavBar() {
             </ul>
         </nav>
     );
-}
+} 

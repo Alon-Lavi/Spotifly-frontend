3fd99db6b8e2ee2a3c89699c5606e3f50cdb862f
amirshamia <amir0988@gmail.com>
2023-10-06 14:47:06 +0300

    added genres list

--- src/cmps/SearchPage.jsx
+++ src/cmps/SearchPage.jsx
@@ -1,45 +1,48 @@
 
 import { Link } from "react-router-dom"
 import { StationPreview } from "./StationPreview"
-import { stationService } from '../services/station.service.js'
+import { stationService } from '../services/station.service.local.js'
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { loadStations, addStation, updateStation, removeStation } from '../store/actions/station.actions.js'
 import { AiOutlineSearch } from 'react-icons/ai'
+import { GenrePreview } from "./GenrePreview"
 
 export function SearchPage() {
 
-    const stations = useSelector((storeState) => storeState.stationModule.stations)
-	useEffect(() => {
-		loadStations()
-	}, [])
-
+    const [genres, setGenres] = useState()
+    useEffect(() => {
+        loadGenres()
+    }, [])
+    async function loadGenres() {
+        setGenres(await stationService.getGenres())
+    }
     return (
         <section className="search-page">
             <div className="search-bar">
-               <form>
-                <h4 className="search-header">Browse all</h4>
-                <div className="search-input-container">
-                 <span>
-                    
-                 </span>
-                 <input
-                  name="txt"
-                  type="text"
-                  placeholder="      &#128269; What do you want to listen to?"
-/>
-                </div>
-               
-               </form>
-            </div>
-            <ul className="station-list">
-        {stations.map((station) => (
-            <li className="station-preview" key={station._id}>
-             <Link to={`station/${station._id}`}>  <StationPreview station={station} /></Link> 
+                <form>
+                    <h4 className="search-header">Browse all</h4>
+                    <div className="search-input-container">
+                        <span>
 
-            </li>
-        ))}
-    </ul>
+                        </span>
+                        <input
+                            name="txt"
+                            type="text"
+                            placeholder="      &#128269; What do you want to listen to?"
+                        />
+                    </div>
+
+                </form>
+            </div>
+            {genres && <ul className="genres-list">
+                {genres.map((genre) => (
+            
+                        <Link to={`station/${genre.title}`}>  <GenrePreview genre={genre} /></Link>
+
+                   
+                ))}
+            </ul>}
         </section>
     )
 }

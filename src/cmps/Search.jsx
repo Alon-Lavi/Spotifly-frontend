2b8a1f541e0a5e551c05c43ac2442c0bd33d3b4c
Daniel Kirushin <retrostyae@mail.ru>
2023-10-09 11:44:46 +0300

    AppHeader/SearchInput/scss

---
+++ src/cmps/Search.jsx
@@ -0,0 +1,27 @@
+
+
+
+
+
+export function Search() {
+
+    return (
+    <div className="search-bar">
+        <form>
+
+            <div className="search-input-container">
+                <span>
+
+                </span>
+                <input
+                    name="txt"
+                    type="text"
+                    placeholder="      &#128269; What do you want to listen to?"
+                />
+            </div>
+
+        </form>
+    </div>
+
+    )
+}

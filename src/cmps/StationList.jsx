8e96f1d6eb00aa23fd8f36ef1df92d638456d058
Daniel Kirushin <retrostyae@mail.ru>
2023-10-23 18:20:10 +0300

    StationList/StationIndex/AppHeader/scss

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -3,23 +3,26 @@ import { StationPreview } from "./StationPreview.jsx"
 import { useSelector } from "react-redux"
 
 
-export function StationList({stations,onPlayStation}){
+export function StationList({ stations, onPlayStation }) {
     const navigate = useNavigate()
-	const user =useSelector((storeState) => storeState.userModule.user)
+    const user = useSelector((storeState) => storeState.userModule.user)
 
-    return(
-        <ul className="station-list">
-        {user&&   <li onClick={()=> navigate(`/likedsongs`)} className="station-preview" key={user.likedSongs._id}>
-           <StationPreview onPlayStation={onPlayStation} station={user.likedSongs} />           
-            
-            </li> }
-        {stations.map((station) => (
-            <li onClick={()=> navigate(`/station/${station._id}`)} className="station-preview" key={station._id}>
- 
-            <StationPreview onPlayStation={onPlayStation} station={station} />           
+    return (
+        <section>
+            <h1 className="episodes-txt">Episodes for you</h1>
+            <ul className="station-list">
+                {user && <li onClick={() => navigate(`/likedsongs`)} className="station-preview" key={user.likedSongs._id}>
+                    <StationPreview onPlayStation={onPlayStation} station={user.likedSongs} />
 
-            </li>
-        ))}
-    </ul>
+                </li>}
+                {stations.map((station) => (
+                    <li onClick={() => navigate(`/station/${station._id}`)} className="station-preview" key={station._id}>
+
+                        <StationPreview onPlayStation={onPlayStation} station={station} />
+
+                    </li>
+                ))}
+            </ul>
+        </section>
     )
 }

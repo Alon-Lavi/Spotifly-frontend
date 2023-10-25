b3c36e3093d8ea657a26a9b7020d0e1517df3f31
Daniel Kirushin <retrostyae@mail.ru>
2023-10-25 18:45:18 +0300

    NavBar >>> Svg-active

--- src/cmps/NavBar.jsx
+++ src/cmps/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { StationPreview } from "./StationPreview";
 import { SearchPage } from "./SearchPage";
 import { stationService } from "../services/station.service.local";
-
+import { Svg } from "./Svg";
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { loadStations, addStation, updateStation, removeStation } from '../store/actions/station.actions.js'
@@ -14,7 +14,8 @@ export function NavBar() {
 
     const location = useLocation();
     const isLoginPage = location.pathname === '/loginsignup';
-
+    const [isHomeActive, setIsHomeActive] = useState(true);
+    const [isSearchActive, setIsSearchActive] = useState(false);
     const [searchText, setSearchText] = useState('');
     const [stations, setStations] = useState();
     const [isHovered, setIsHovered] = useState(false);
@@ -31,12 +32,15 @@ export function NavBar() {
         const MyStations = await stationService.query(user)
         setStations(MyStations)
     }
-    const handleMouseEnter = () => {
-        setIsHovered(true);
+
+    const handleHomeClick = () => {
+        setIsHomeActive(true);
+        setIsSearchActive(false);
     };
 
-    const handleMouseLeave = () => {
-        setIsHovered(false);
+    const handleSearchClick = () => {
+        setIsHomeActive(false);
+        setIsSearchActive(true);
     };
     async function createStation() {
         const station = await stationService.getEmptyStation(user)
@@ -56,24 +60,13 @@ export function NavBar() {
         <nav className="side-bar">
             <ul className="side-bar-list">
                 <div className="header-side-bar">
-                    <li className={`side-bar-item-home ${isHovered ? 'hovered' : ''}`}>
+                    <li className={`side-bar-item-home ${isHomeActive ? 'active' : ''}`}>
                         <NavLink
                             to="/"
                             className="nav-link flex"
-                            onMouseEnter={handleMouseEnter}
-                            onMouseLeave={handleMouseLeave}
+                            onClick={handleHomeClick}
                         >
-                            <svg
-                                role="img"
-                                height="23"
-                                width="26"
-                                className="icon"
-                                viewBox="-1 0 25 22"
-                                data-encore-id="icon"
-                                style={{ fill: isHovered ? 'white' : '#b3b3b3', marginRight: '15px', marginBottom: '-15px' }}
-                            >
-                                <path d="M 12.5 3.247 a 1 1 0 0 0 -1 0 L 4 7.577 V 20 h 4.5 v -6 a 1 1 0 0 1 1 -1 h 5 a 1 1 0 0 1 1 1 v 6 H 20 V 7.577 l -7.5 -4.33 Z m -2 -1.732 a 3 3 0 0 1 3 0 l 7.5 4.33 a 2 2 0 0 1 1 1.732 V 21 a 1 1 0 0 1 -1 1 h -6.5 a 1 1 0 0 1 -1 -1 v -6 h -3 v 6 a 1 1 0 0 1 -1 1 H 3 a 1 1 0 0 1 -1 -1 V 7.577 a 2 2 0 0 1 1 -1.732 l 7.5 -4.33 Z"></path>
-                            </svg>
+                            {isHomeActive ? Svg.activeHome : Svg.homeIcon}
                             <div className="txt-home">
                                 Home
                             </div>
@@ -81,24 +74,10 @@ export function NavBar() {
 
                         </NavLink>
                     </li>
-                    <li className={`side-bar-item ${isHovered ? 'hovered' : ''}`}>
-                        <NavLink to="/search" className="nav-link flex" >
-                            <svg
-                                role="img"
-                                height="24"
-                                width="26"
-                                aria-hidden="true"
-                                className="icon"
-                                viewBox="0 0 23 24"
-                                data-encore-id="icon"
-                                style={{ fill: "#b3b3b3", marginRight: '15px', marginBottom: '1px' }}
-                            >
-                                <path d="M15.356 10.558c0 2.623-2.16 4.75-4.823 4.75-2.664 0-4.824-2.127-4.824-4.75s2.16-4.75 4.824-4.75c2.664 0 4.823 2.127 4.823 4.75z"></path><path d="M1.126 10.558c0-5.14 4.226-9.28 9.407-9.28 5.18 0 9.407 4.14 9.407 9.28a9.157 9.157 0 0 1-2.077 5.816l4.344 4.344a1 1 0 0 1-1.414 1.414l-4.353-4.353a9.454 9.454 0 0 1-5.907 2.058c-5.18 0-9.407-4.14-9.407-9.28zm9.407-7.28c-4.105 0-7.407 3.274-7.407 7.28s3.302 7.279 7.407 7.279 7.407-3.273 7.407-7.28c0-4.005-3.302-7.278-7.407-7.278z"></path></svg>
-                            <div className="txt-search">
-
-                                Search
-                            </div>
-
+                    <li className={`side-bar-item-search ${isSearchActive ? 'active' : ''}`}>
+                        <NavLink to="/search" className="nav-link flex" onClick={handleSearchClick}>
+                            {isSearchActive ? Svg.activeSearch : Svg.searchHomePageIcon}
+                            <div className="txt-search">Search</div>
                         </NavLink>
                     </li>
                 </div>
